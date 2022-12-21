ec63815036e30bbb41b5207bd257163fcf4248f4
Emdad5353 <myselfemdad@gmail.com>
2022-12-21 03:42:58 +0000

    first input

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Sylhet Nursery</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700"
+        rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+    <!--==========================
+  Header
+  ============================-->
+    <header id="header">
+        <div class="container">
+
+            <div id="logo" class="pull-left">
+                <a href="#hero"><img src="img/logo.svg"  alt="SYLHET NURSERY" title="" /></img></a>
+                <!-- text logo -->
+                <!-- <h3 ><a href="#hero" class="logo-color" >SYLHET NURSERY</a></h3> -->
+
+                
+            </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="#hero">Home</a></li>
+                    <li><a href="#about">About Us</a></li>
+
+
+                    <li><a href="#contact">Contact Us</a></li>
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </header><!-- #header -->
+
+    <!--==========================
+    Hero Section
+  ============================-->
+    <section id="hero">
+        <div class="hero-container">
+            <h1>Sylhet Nursery</h1>
+            <h2>Seedling of good quality</h2>
+            <a href="#about" class="btn-get-started">About Us</a>
+        </div>
+    </section><!-- #hero -->
+
+    <main id="main">
+
+        <!--==========================
+      About Us Section
+    ============================-->
+        <section id="about">
+            <div class="container">
+                <div class="row about-container">
+
+                    <div class="col-lg-6 content order-lg-1 order-2">
+                        <h2 class="title">Few Words About Us</h2>
+                        <p>
+                        Sylhet Nursery is one of the best and popular Nursery in Sylhet. We cultivate vegetable trees, medical trees, flower trees. We have lots of different kinds of plants like mango, peanut, apple, litchi, pepper, papaya, tomatoes, neem, mahogany, teak, cork, orchids, hibiscus, sunflowers, roses, etc.
+                        </p>
+
+                        <div class="icon-box wow fadeInUp">
+                            <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+                            <h4 class="title"><a href="">Available </a></h4>
+                            <p class="description">
+                                Hours
+                                <ul>
+                                    <li> Monday 8AM–4PM </li>
+                                    <li> Tuesday 8AM–4PM </li>
+                                    <li> Wednesday 8AM–4PM </li>
+                                    <li> Thursday 8AM–4PM </li>
+                                    <li> Friday 8AM–4PM </li>
+                                    <li> Saturday 8AM–4PM </li>
+                                    <li> Sunday 8AM–4PM </li>
+                                </ul>
+                            </p>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+
+                    <br>
+
+
+                   
+                    <div class="col-lg-12 order-lg-3 order-3 fadeInLeft text-center">
+                    <br><br>
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+                                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                                
+
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="img/slider/01.jpg" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/02.jpg" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/03.jpg" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/04.jpg" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/05.jpg" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/06.jpg" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/07.jpg" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="img/slider/08.jpg" alt="">
+                                </div>
+
+
+                              
+                            </div>
+
+
+
+                            <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                                data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                                data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a> -->
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+        </section><!-- #about -->
+
+
+        <!--==========================
+    Call To Action Section
+    ============================-->
+        <section id="call-to-action">
+            <div class="container wow fadeIn">
+                <div class="row">
+                    <div class="col-lg-9 text-center text-lg-left">
+                        <h3 class="cta-title">Quality Seedlings</h3>
+                        <p class="cta-text">Here you get all kinds of outdoor trees, medicinal plants & flower
+                            seedlings. </p>
+                    </div>
+                    <!-- <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#">Call To Action</a>
+          </div> -->
+                </div>
+
+            </div>
+        </section><!-- #call-to-action -->
+
+
+        <!--==========================
+      Contact Section
+    ============================-->
+        <section id="contact">
+            <div class="container wow fadeInUp">
+                <div class="section-header">
+                    <h3 class="section-title">Contact</h3>
+                    <p class="section-description">Feel free to contact us. </p>
+                </div>
+            </div>
+
+            <!-- Uncomment below if you wan to use dynamic maps -->
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28951.95462563443!2d91.88118482428258!3d24.89817509409128!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8d72e8eea35f0ac3!2sSylhet+Nursery!5e0!3m2!1sen!2sus!4v1555967255023!5m2!1sen!2sus"
+                width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+            <div class="container wow fadeInUp mt-5">
+                <div class="row justify-content-center">
+
+                    <div class="col-lg-3 col-md-4">
+
+                        <div class="info">
+                            <div>
+                                <i class="fa fa-map-marker"></i>
+                                <p>Old Medical, Dariapara Road<br>Chowhatta, Sylhet 3100</p>
+                            </div>
+
+                            <div>
+                                <i class="fa fa-envelope"></i>
+                                <p>info@sylhetnursery.com</p>
+                            </div>
+
+                            <div>
+                                <i class="fa fa-phone"></i>
+                                <p>+880 1643-309662</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div class="col-lg-5 col-md-8">
+                        <div class="form">
+                            <div id="sendmessage">Your message has been sent. Thank you!</div>
+                            <div id="errormessage"></div>
+                            <form action="" method="post" role="form" class="contactForm">
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" id="name"
+                                        placeholder="Your Name" data-rule="minlen:4"
+                                        data-msg="Please enter at least 4 chars" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" name="email" id="email"
+                                        placeholder="Your Email" data-rule="email"
+                                        data-msg="Please enter a valid email" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="subject" id="subject"
+                                        placeholder="Subject" data-rule="minlen:4"
+                                        data-msg="Please enter at least 8 chars of subject" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" name="message" rows="5" data-rule="required"
+                                        data-msg="Please write something for us" placeholder="Message"></textarea>
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="text-center"><button type="submit">Send Message</button></div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- #contact -->
+
+    </main>
+
+    <!--==========================
+    Footer
+  ============================-->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong>Sylhet Nursary</strong>, 2017.
+            </div>
+
+        </div>
+        <div class="credits">
+
+            All Rights Reserved
+        </div>
+
+    </footer><!-- #footer -->
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+
+    <!-- JavaScript Libraries -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+
+    <!-- Contact Form JavaScript File -->
+    <script src="contactform/contactform.js"></script>
+
+    <!-- Template Main Javascript File -->
+    <script src="js/main.js"></script>
+
+
+
+</body>
+
+</html>
